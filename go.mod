482074d65b8fd21a83b9003fd6ed32419b7ed129
Mostafa Solati <mostafa.solati@gmail.com>
2022-07-16 23:59:12 +0430

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mostafasolati/t2
+
+go 1.19
---
+++ translation.go
@@ -0,0 +1,17 @@
+package translation
+
+import (
+	"github.com/mostafasolati/t2/english"
+	"github.com/mostafasolati/t2/persian"
+)
+
+func TranslateWeekDay(lang string, day int) string {
+	switch lang {
+	case "fa":
+		return persian.TranslateWeekDay(day)
+	case "en":
+		return english.TranslateWeekDay(day)
+	default:
+		return "نامشخصی"
+	}
+}
